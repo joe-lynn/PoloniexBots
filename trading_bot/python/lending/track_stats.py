32b64d420d60e6e781495308b96b78a09261c3e9
STFinancial <palla021@umn.edu>
2016-08-06 08:42:07 -0700

    Working on stats writer, need to flesh out the public fetcher so all other threads can easily get all their necessary information

--- trading_bot/python/lending/track_stats.py
+++ trading_bot/python/lending/track_stats.py
@@ -1,11 +1,26 @@
+import api.public_fetcher
+import os
+import signal
+import sys
 import threading
 
 
+sys.path.append("/Users/Timothy/Code/Poloniex/trading_bot/python")
+
 class stat_writer(threading.Thread):
-	def __init__(self):
+	def write_stats(self):
+		lock.acquire()
+		# TODO(pallarino): Loop through loan currencies and write to respective files.
+		fetcher.get_loan_offers()
+		rates_file.write()
+		lock.release()
+
+	def __init__(self, fetcher):
 		super().__init__()
-		self.rates_file = open("~/Code/trading_bot/min_rates.dat", 'a')
+		self.rates_file = open("./data/min_rates.dat", 'a')
 		self.lock = threading.Lock()
+		signal.signal(signal.SIGALRM, write_stats)
+		self.fetcher = fetcher
 
 	def run(self):
-		
+		time.sleep(0)
