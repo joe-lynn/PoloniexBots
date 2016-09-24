ae6939d8c01b6ed401eee3babe363a4964de39e1
STFinancial <palla021@umn.edu>
2016-09-24 00:24:58 -0700

    Changes to gitignore.

--- lending_bot/python/run.py
+++ lending_bot/python/run.py
@@ -1,6 +1,7 @@
+import os
 import sys
 
-sys.path.insert(0, '/Users/Timothy/Code/PoloniexBots/lending_bot/python')
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 from bots.naive_lender import NaiveLender
 from api.poloniex_wrapper import poloniex
