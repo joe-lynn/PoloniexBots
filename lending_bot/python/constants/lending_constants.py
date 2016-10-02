8e3ae604d35dc853017fcd96f4dd0f0087b71c02
STFinancial <palla021@umn.edu>
2016-10-01 19:22:11 -0700

    Working on the naive bot

---
+++ lending_bot/python/constants/lending_constants.py
@@ -0,0 +1,13 @@
+MIN_AMOUNT = 0.001
+MIN_RATE = 0.000001
+MAX_RATE = 0.05
+MIN_DURATION = 2
+MAX_DURATION = 60
+
+LOAN_CURRENCIES = ['BTC', 'BTS', 'CLAM', 
+				   'DASH', 'DOGE', 'ETH', 
+				   'FCT', 'LTC', 'MAID', 
+				   'STR', 'XMR', 'XRP',]
+
+# Poloniex takes a 15% cut on all earned interest.
+POLO_CUT = 0.15
